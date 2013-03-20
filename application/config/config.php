7c8b88a54974144f5553da76f24aedcff2fa8082
Breinn Tan Aguilar <breinntaguilar@gmail.com>
2013-03-20 16:55:08 +0800

    changed base_url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://' . $_SERVER['SERVER_NAME'] . '/eteeap_project/MIS';
+$config['base_url']	= 'http://' . $_SERVER['SERVER_NAME'] . '/MIS';
 
 /*
 |--------------------------------------------------------------------------
